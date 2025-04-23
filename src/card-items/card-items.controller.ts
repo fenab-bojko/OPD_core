d9ac432ee7a852f0680860e791c77584818f5759
Boyko Andrey <boyko.a@compmechlab.ru>
2025-04-23 11:07:01 +0300

    Correct

--- src/card-items/card-items.controller.ts
+++ src/card-items/card-items.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
-import { CardItemsService } from './card-items.service';
-import { ICardItem } from './types';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { CardItemsService } from "./card-items.service";
+import { ICardItem } from "./types";
 
-@Controller('projects')
+@Controller("projects")
 export class CardItemsController {
   constructor(private readonly cardItemsService: CardItemsService) {}
 
@@ -11,8 +11,8 @@ export class CardItemsController {
     return this.cardItemsService.findAll();
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
+  @Get(":id")
+  async findOne(@Param("id") id: string) {
     return this.cardItemsService.findOne(id);
   }
 
@@ -21,18 +21,18 @@ export class CardItemsController {
     return this.cardItemsService.create(body);
   }
 
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
+  @Delete(":id")
+  async delete(@Param("id") id: string) {
     return this.cardItemsService.delete(id);
   }
 
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() body: ICardItem) {
+  @Put(":id")
+  async update(@Param("id") id: string, @Body() body: ICardItem) {
     return this.cardItemsService.update(id, body);
   }
 
-  @Patch(':id')
-  async patch(@Param('id') id: string, @Body() body: ICardItem) {
+  @Patch(":id")
+  async patch(@Param("id") id: string, @Body() body: ICardItem) {
     return this.cardItemsService.patch(id, body);
   }
 }
